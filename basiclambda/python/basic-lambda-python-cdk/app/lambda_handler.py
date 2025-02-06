f10eb30d7c5c4a55e1435a787b27c40aca0be880
c3d4r <c3d4r.wood@gmail.com>
2025-02-06 21:28:48 +0200

    add basic handler

---
+++ basiclambda/python/basic-lambda-python-cdk/app/lambda_handler.py
@@ -0,0 +1,10 @@
+import json
+
+print("Loading function")
+
+def lambda_handler(event, context):
+    print(f"Received event: {event}")
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello world from basic-lambda-python-cdk Lambda!')
+    }

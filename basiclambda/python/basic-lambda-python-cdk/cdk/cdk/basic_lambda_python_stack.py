4133ca633851e8d096cb3918f759aedb528b806c
c3d4r <c3d4r.wood@gmail.com>
2025-02-06 22:17:09 +0200

    Add FunctionUrl to CDK

--- basiclambda/python/basic-lambda-python-cdk/cdk/cdk/basic_lambda_python_stack.py
+++ basiclambda/python/basic-lambda-python-cdk/cdk/cdk/basic_lambda_python_stack.py
@@ -16,7 +16,10 @@ class BasicLambdaPythonStack(Stack):
                          code=lambda_.Code.from_asset("../app")
                          )
 
+        fn_url = fn.add_function_url(auth_type=lambda_.FunctionUrlAuthType.NONE)
+
         CfnOutput(self, "functionArn", value=fn.function_arn, description="The ARN of the provisioned function")
+        CfnOutput(self, "functionUrl", value=fn_url.url, description="The URL of the provisioned function")
 
 
 

f09422df647bc8d33245f1e2ff066648cd493162
c3d4r <c3d4r.wood@gmail.com>
2025-02-08 21:16:54 +0200

    add implementation for basic-lambda-python-pulumi

--- basiclambda/python/basic-lambda-python-pulumi/pulumi/__main__.py
+++ basiclambda/python/basic-lambda-python-pulumi/pulumi/__main__.py
@@ -1,10 +1,45 @@
-"""An AWS Python Pulumi program"""
-
 import pulumi
-from pulumi_aws import s3
+from pulumi_aws import lambda_, iam
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.BucketV2('my-bucket')
+assume_role = iam.get_policy_document(statements=[{
+    "effect": "Allow",
+    "principals": [{
+        "type": "Service",
+        "identifiers": ["lambda.amazonaws.com"],
+    }],
+    "actions": ["sts:AssumeRole"],
+}])
+iam_for_lambda = iam.Role("iam_for_lambda",
+    name="basic-lambda-python-pulumi-role",
+    assume_role_policy=assume_role.json)
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+fn = lambda_.Function('basic-lambda-python-pulumi',
+                      handler="lambda_handler.lambda_handler",
+                      code= pulumi.AssetArchive({".": pulumi.FileArchive("../app")}),
+                      role=iam_for_lambda.arn,
+                      runtime="python3.12",
+                      )
+
+# Export the ARN of the function
+pulumi.export('lambda_arn', fn.arn)
+
+fn_url = lambda_.FunctionUrl("basic-lambda-python-pulumi",
+    function_name=fn.name,
+    authorization_type="NONE",
+    cors={
+        "allow_credentials": True,
+        "allow_origins": ["*"],
+        "allow_methods": ["*"],
+        "allow_headers": [
+            "date",
+            "keep-alive",
+        ],
+        "expose_headers": [
+            "keep-alive",
+            "date",
+        ],
+        "max_age": 86400,
+    })
+
+# Export the URL of the function
+pulumi.export('function_url', fn_url.function_url)
